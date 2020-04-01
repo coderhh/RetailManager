73b115abfd835b26671b65b8322783a628f13783
coderhh <yehanghan@gmail.com>
2020-04-01 14:49:31 +0800

    Create Register DB Table
    Adds tables for simple register

---
+++ RMData/dbo/Tables/SaleDetail.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[SaleDetail]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [SaleId] INT NOT NULL, 
+    [ProductId] INT NOT NULL, 
+    [Quantity] INT NOT NULL DEFAULT 1,
+    [PurchasePrice] MONEY NOT NULL, 
+    [Tax] NCHAR(10) NOT NULL DEFAULT 0, 
+    
+
+)

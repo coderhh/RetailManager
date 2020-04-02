54f7e4900aa933927fd112887106b522dee5c025
coderhh <yehanghan@gmail.com>
2020-04-02 10:06:55 +0800

    Add Login Form
    Adds a  login usercontrol to the project

--- RMDesktopUI/Bootstrapper.cs
+++ RMDesktopUI/Bootstrapper.cs
@@ -3,13 +3,11 @@ using RMDesktopUI.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 
 namespace RMDesktopUI
 {
-    public class Bootstrapper: BootstrapperBase
+    public class Bootstrapper : BootstrapperBase
     {
         private SimpleContainer _container = new SimpleContainer();
         public Bootstrapper()
---
+++ RMDesktopUI/ViewModels/LoginViewModel.cs
@@ -0,0 +1,46 @@
+﻿using Caliburn.Micro;
+using System;
+
+namespace RMDesktopUI.ViewModels
+{
+    public class LoginViewModel : Screen
+    {
+        private string _userName;
+        private string _password;
+
+        public string UserName
+        {
+            get { return _userName; }
+            set
+            {
+                _userName = value;
+                NotifyOfPropertyChange(() => UserName);
+                NotifyOfPropertyChange(() => CanLogIn);
+            }
+        }
+
+        public string Password
+        {
+            get { return _password; }
+            set
+            {
+                _password = value;
+                NotifyOfPropertyChange(() => Password);
+                NotifyOfPropertyChange(() => CanLogIn);
+            }
+        }
+
+        public bool CanLogIn
+        {
+            get
+            {
+                return (!String.IsNullOrEmpty(UserName) && !String.IsNullOrEmpty(Password));
+            }
+        }
+
+        public void LogIn()
+        {
+            Console.WriteLine("Login!" + UserName + Password);
+        }
+    }
+}

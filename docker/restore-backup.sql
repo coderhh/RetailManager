ac08e3f02b3ef1703fac3c06bac8657fb5c4b254
coderhh <yehanghan@gmail.com>
2020-04-29 23:40:53 +0800

    Add Docker Support
    docker sql server test

---
+++ docker/restore-backup.sql
@@ -0,0 +1,7 @@
+RESTORE DATABASE [AdventureWorks] FROM DISK = '/tmp/AdventureWorksLT2017.bak'
+WITH FILE = 1,
+MOVE 'AdventureWorksLT2012_Data' TO '/var/opt/mssql/data/AdventureWorks.mdf',
+MOVE 'AdventureWorksLT2012_Log' TO '/var/opt/mssql/data/AdventureWorks.ldf',
+NOUNLOAD, REPLACE, STATS = 5
+GO
+
